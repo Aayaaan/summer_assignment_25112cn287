fe9dadddbe1d96dd766dec092a83f803f8670a42
Sufiyan Ansari <sa00789000@gmail.com>
2026-06-07 11:58:31 +0530

    day01 again

--- day01/q01.java
+++ day01/q01.java
@@ -1,8 +1,9 @@
 public class q01 {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int sum = a + b;
-        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+      int sum=0,n=10;
+      for(int i=1;i<=n;i++){
+        sum= sum + i;
+      }
+      System.out.println("The sum of first " + n + " natural numbers is: " + sum);
     }
 }
