c3fc02d2fcf179b6af673733afc6d310e42510e3
Sufiyan Ansari <sa00789000@gmail.com>
2026-06-08 12:57:06 +0530

    q01 done

--- day02/q01.java
+++ day02/q01.java
@@ -1,4 +1,9 @@
 public class q01 {
     public static void main(String[] args) {
-        system.out.println("Hello, World!");
-    }
+        int num = 12345, sum = 0;
+        while(num != 0){
+            sum = sum + (num % 10);
+            num = num / 10;
+        }
+        System.out.println(sum);
+    }}
