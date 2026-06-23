6431cc583f33be4f944514e9eedadddfd82be79d
Sufiyan Ansari <sa00789000@gmail.com>
2026-06-23 19:20:01 +0530

    day13 completed

---
+++ day13/q04.java
@@ -0,0 +1,20 @@
+public class q04 {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 15, 20, 25, 30};
+
+        int even = 0;
+        int odd = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+
+        System.out.println("Even = " + even);
+        System.out.println("Odd = " + odd);
+    }
+}
