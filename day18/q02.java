7765802882dc6294faf0cbd385251e9c0b8865ce
Sufiyan Ansari <sa00789000@gmail.com>
2026-06-25 19:44:34 +0530

    day18 completed

---
+++ day18/q02.java
@@ -0,0 +1,27 @@
+public class q02 {
+    public static void main(String[] args) {
+
+        int[] arr = {5, 3, 8, 4, 2};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            int min = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+
+        System.out.print("Sorted Array: ");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
---
+++ day18/q04.java
@@ -0,0 +1,25 @@
+public class q04 {
+    public static void main(String[] args) {
+
+        int[] arr = {5, 3, 8, 4, 2};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] < arr[j]) {
+
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.print("Descending Order: ");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
