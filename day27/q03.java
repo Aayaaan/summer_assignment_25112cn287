66bffa1c3548d5a693966898e248e15c5e67d1f3
Sufiyan Ansari <sa00789000@gmail.com>
2026-06-28 19:01:15 +0530

    day27 completed

---
+++ day27/q03.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class q03 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Basic Salary: ");
+        double basic = sc.nextDouble();
+
+        double hra = basic * 0.20;
+        double da = basic * 0.10;
+        double totalSalary = basic + hra + da;
+
+        System.out.println("\n--- Salary Details ---");
+        System.out.println("Basic Salary: " + basic);
+        System.out.println("HRA: " + hra);
+        System.out.println("DA: " + da);
+        System.out.println("Total Salary: " + totalSalary);
+
+        sc.close();
+    }
+}
