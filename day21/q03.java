5f768f2c5b7193200f1b8ee3f4f5417b05f1fd74
Sufiyan Ansari <sa00789000@gmail.com>
2026-06-28 15:11:50 +0530

    day21 completed

---
+++ day21/q03.java
@@ -0,0 +1,22 @@
+public class q03 {
+    public static void main(String[] args) {
+
+        String str = "Hello";
+
+        int vowels = 0;
+        int consonants = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = Character.toLowerCase(str.charAt(i));
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                vowels++;
+            else
+                consonants++;
+        }
+
+        System.out.println("Vowels = " + vowels);
+        System.out.println("Consonants = " + consonants);
+    }
+}
---
+++ day21/q04.java
@@ -0,0 +1,10 @@
+public class q04 {
+    public static void main(String[] args) {
+
+        String str = "hello";
+
+        String upper = str.toUpperCase();
+
+        System.out.println("Uppercase = " + upper);
+    }
+}
